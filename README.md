413bcbf3b15613539a5bbaaac88d51fe06123f4c
hosumihaidarius <hosumihaidarius@gmail.com>
2021-11-26 12:13:27 +0200

    added tests for almost all the menu items.
    assertions on each individual page is done only by checking the URL.
    more assertions like checking header will follow.

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # heits-website-e2e-tests
 
-CLI run commands : \
-_pytest_ to run in headless \
-_pytest --headed_ to run in headed mode with browser open
+CLI run commands :
+
+| `pytest` | run all the tests in headless mode \
+| `pytest tests/test_home.py ` | run all the tests in headless mode from specified .py file
+
+Information regarding [CLI commands on playwright](https://playwright.dev/python/docs/test-runners/) e.g:
+
+| arg names             | Description                           |
+|------------------|---------------------------------------|
+| `--browser` | specifies browser type e.g. chrome, chromium, firefox|
+| `--slowmo` | runs the tests in slow motion e.g. --slowmo=500  |
+| `--device` | runs the tests on specified device e.g. --device=iPhone 11  |
--- utils/data.py
+++ utils/data.py
@@ -1,4 +1,10 @@
 from enum import Enum
+from aenum import MultiValueEnum
+
+
+class Urls(Enum):
+    BASE_URL = 'https://apex.heits.digital/'
+    ENGINEERING_URL = 'https://apex.heits.digital/engineering'
 
 
 class MenuCloseHeader(Enum):
@@ -6,6 +12,17 @@ class MenuCloseHeader(Enum):
     CLOSE = 'Close'
 
 
-class Urls(Enum):
-    BASE_URL = 'https://apex.heits.digital/'
-    ENGINEERING_URL = 'https://apex.heits.digital/engineering'
+class MenuItems(MultiValueEnum):
+    """
+    MultiValueEnum enables tuple values in enum configuration
+    e.g. MenuItems.Machine_Learning.values[0] returns 'Machine learning'
+    e.g. MenuItems.Machine_Learning.values[1] returns 'https://apex.heits.digital/machine-learning'
+    """
+    MACHINE_LEARNING = 'Machine learning', 'https://apex.heits.digital/machine-learning'
+    DIGITAL_SOFTWARE_DEVELOPMENT = 'Digital software development', 'https://apex.heits.digital/software-development'
+    USER_EXPERIENCE = 'User experience', 'https://apex.heits.digital/user-experience'
+    CULTURE = 'Culture', 'https://apex.heits.digital/culture'
+    OUR_WORK = 'Our work', 'https://apex.heits.digital/work'
+    ARTICLES = 'Articles', 'https://apex.heits.digital/articles'
+    ENGAGEMENT_MODELS = 'Engagement models', 'https://apex.heits.digital/engagement'
+    LETS_CHAT = 'Let’s chat'
